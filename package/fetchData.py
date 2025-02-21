8f486a1a4969f9ca6f5af12b9d39913f9b220404
nareshix <nareshix66@gmail.com>
2025-02-21 18:48:42 +0800

    reduced boilerplate

--- package/fetchData.py
+++ package/fetchData.py
@@ -13,26 +13,8 @@ response = requests.get(repos)
 data = json.loads(response.text)
 
 
-def show_error():
-    print(f"Failed to retrieve the file. Status code: {response.status_code}")
-
-
-def get_download_link(app):
+def get(app,attribute):
     if response.status_code == 200:
-        return data[app]["download_link"]
+        return data[app][attribute]
     else:
-        show_error()
-
-def get_symlink(app):
-    if response.status_code == 200:
-        return data[app]["symlink"]
-    else:
-        show_error()
-
-def check_multiple_files(app):
-    if response.status_code == 200:
-        return data[app]["multiple_files"]
-    else:
-        show_error()
-
-
+        print(f"Failed to retrieve the file. Status code: {response.status_code}")
