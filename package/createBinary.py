9134f9f8b891b4417474d2a6503c9bdfbc15ebd6
nareshix <nareshix66@gmail.com>
2025-02-22 09:40:40 +0800

    handle official ppas first, later worry about binaries and added an alias dict

--- package/createBinary.py
+++ package/createBinary.py
@@ -5,26 +5,42 @@ import subprocess
 home_dir = os.path.expanduser('~')
 STORAGE_FOLDER_PATH = home_dir + '/' + '.naresh' # $HOME/.naresh
 
+ALIAS = {
+    "codium": "vscodium"
+}
+
 def createBinary(app):
-    if not os.path.exists(STORAGE_FOLDER_PATH):
-        os.makedirs(STORAGE_FOLDER_PATH)
-        
-    APP_PATH = STORAGE_FOLDER_PATH + '/' + app
-    if not os.path.exists(APP_PATH):
-        os.makedirs(APP_PATH)
+    if app in ALIAS:
+        app = ALIAS[app]
 
-    website_url = get(app,"download_link")
-
-    #Downloads file
-    subprocess.run(['wget',  website_url ])
-
-    fileName = website_url.split('/')[-1]
-
-    #extracts the file to local binary
-    subprocess.run(["sudo", "tar", "-xvzf",fileName , "-C", APP_PATH])
+    if get(app,"official_ppa"):  #if offical ppa exists, use that instead
+        for command in get(app,"download"):
+            subprocess.run(command, shell=True, check=True)
+        return
     
-    os.remove(fileName)     
 
-    #Create a symlink (available to use anywhere in terminal)
-    symlink = get(app,'symlink')
-    subprocess.run(['sudo', 'ln', '-s', f'{APP_PATH}/{symlink}' , f"/usr/local/bin/{symlink}"])  
+
+
+
+#     if not os.path.exists(STORAGE_FOLDER_PATH):
+#         os.makedirs(STORAGE_FOLDER_PATH)
+        
+#     APP_PATH = STORAGE_FOLDER_PATH + '/' + app
+#     if not os.path.exists(APP_PATH):
+#         os.makedirs(APP_PATH)
+
+#     website_url = get(app,"download_link")
+
+#     #Downloads file
+#     subprocess.run(['wget',  website_url ])
+
+#     fileName = website_url.split('/')[-1]
+
+#     #extracts the file to local binary
+#     subprocess.run(["sudo", "tar", "-xvzf",fileName , "-C", APP_PATH])
+    
+#     os.remove(fileName)     
+
+#     #Create a symlink (available to use anywhere in terminal)
+#     symlink = get(app,'symlink')
+#     subprocess.run(['sudo', 'ln', '-s', f'{APP_PATH}/{symlink}' , f"/usr/local/bin/{symlink}"])  
