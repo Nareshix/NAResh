985df5226b15df61ee6b37e35edbe5ce3bea13ba
nareshix <nareshix66@gmail.com>
2025-02-22 10:30:59 +0800

    added deleted repos and a del func

--- package/fetchData.py
+++ package/fetchData.py
@@ -8,13 +8,24 @@ import json
 
 
 repos = "https://raw.githubusercontent.com/Nareshix/repos/refs/heads/main/apps.json"
+delete_repos = "https://raw.githubusercontent.com/Nareshix/repos/refs/heads/main/delete_apps.json"
 
-response = requests.get(repos)
-data = json.loads(response.text)
 
 
 def get(app,attribute):
+    response = requests.get(repos)
+    data = json.loads(response.text)
+
     if response.status_code == 200:
         return data[app][attribute]
+    else:
+        print(f"Failed to retrieve the file. Status code: {response.status_code}")
+
+def delete(app):
+    response = requests.get(delete_repos)
+    data = json.loads(response.text)
+
+    if response.status_code == 200:
+        return data[app]
     else:
         print(f"Failed to retrieve the file. Status code: {response.status_code}")
